084a0f93f69737019b7cee8b167ec5a5580505f9
Alex Carrera <a_fcalvarez@hotmail.com>
2020-06-14 12:39:11 -0500

    solucion errores not defined

--- rickyMorty/src/app/query/query.component.ts
+++ rickyMorty/src/app/query/query.component.ts
@@ -11,6 +11,10 @@ export class QueryComponent implements OnInit {
 
   conversiones: any;
   nombre: string;
+  origin: string;
+  species: string;
+  status: string;
+  picture: any;
   rta: any[];
 
   constructor(
@@ -19,16 +23,22 @@ export class QueryComponent implements OnInit {
 
 
   ngOnInit(): void {
-  this.llamarString();
+  this.llamarString(1);
   }
 
   // Llamada a la Api para obtener JSON de datos
 
-  llamarString(): void {
+  llamarString(id: number): void {
+
     this.http.get('https://rickandmortyapi.com/api/character')
       .subscribe((data: any) => {
         this.conversiones = data.results;
         this.rta = this.conversiones;
+        this.picture = this.rta[0].image;
+        this.nombre = this.rta[0].name;
+        this.origin = this.rta[0].origin.name;
+        this.species = this.rta[0].species;
+        this.status = this.rta[0].status;
       });
   }
 }
